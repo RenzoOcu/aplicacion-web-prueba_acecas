967c5629beb744e3fafab4ca9823d5ad55c1d65d
RenzoCoder <renzoocupaaliaga@gmail.com>
2024-09-28 20:25:32 -0500

    Add files via upload

---
+++ vistas/menu.php
@@ -0,0 +1,208 @@
+   <aside class="main-sidebar sidebar-dark-primary elevation-4">
+       <!-- Brand Logo -->
+       <a href="index3.html" class="brand-link">
+
+           <span class="brand-text font-weight-light">sistema de ventas</span>
+       </a>
+
+
+
+       <!-- Sidebar -->
+       <div class="sidebar">
+           <!-- Sidebar user panel (optional) -->
+        
+
+           <!-- SidebarSearch Form -->
+           <div class="form-inline">
+               <div class="input-group" data-widget="sidebar-search">
+                   <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                       aria-label="Search">
+                   <div class="input-group-append">
+                       <button class="btn btn-sidebar">
+                           <i class="fas fa-search fa-fw"></i>
+                       </button>
+                   </div>
+               </div>
+           </div>
+
+           <!-- Sidebar Menu -->
+           <nav class="mt-2">
+               <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                   data-accordion="false">
+                   <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+                   <li class="nav-item">
+                    <!-- pruebaaaaaaaaaaaaaaaaaaaaaaaaaaaa -->
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-th"></i>
+                           <p>
+                               tablero
+                               <span class="right badge badge-danger">New</span>
+                           </p>
+                       </a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               almacen
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href="../vistas/articulos.php"" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>articulos</p>
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="../vistas/categorias.php" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>categorias</p>
+                               </a>
+                           </li>
+
+
+                       </ul>
+                   </li>
+
+
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               compras
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href=" aun no ta programado xd " class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>ingresos</p>
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="../vistas/proveedores.php" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>proveedores</p>
+                               </a>
+                           </li>
+
+
+                       </ul>
+                   </li>
+
+
+
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               ventas
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href="aun no esta programado -------" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>ventas</p>
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>clientes</p>
+                               </a>
+                           </li>
+
+
+                       </ul>
+                   </li>
+
+
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               permisos
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href="../vistas/usuario.php" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>usuarios</p>
+                               </a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="../vistas/permisos.php" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>permisos</p>
+                               </a>
+                           </li>
+
+
+                       </ul>
+                   </li>
+
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               consultar compras
+
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href="pages/layout/top-nav.html" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>consultar compras</p>
+                               </a>
+                           </li>
+
+
+
+                       </ul>
+                   </li>
+
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           <i class="nav-icon fas fa-copy"></i>
+                           <p>
+                               consultar ventas
+
+                           </p>
+                       </a>
+                       <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                               <a href="pages/layout/top-nav.html" class="nav-link">
+                                   <i class="far fa-circle nav-icon"></i>
+                                   <p>consultar ventas</p>
+                               </a>
+                           </li>
+
+
+
+                       </ul>
+                   </li>
+
+
+
+
+
+
+
+
+               </ul>
+           </nav>
+           <!-- /.sidebar-menu -->
+       </div>
+       <!-- /.sidebar -->
+   </aside>
