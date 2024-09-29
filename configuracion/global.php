734f8a2120cfdf16ee39e03389df0a652e444c6c
RenzoCoder <renzoocupaaliaga@gmail.com>
2024-09-28 20:24:33 -0500

    Add files via upload

---
+++ configuracion/global.php
@@ -0,0 +1,32 @@
+<?php
+
+
+//conexion de la base de datos (usuario)
+
+define("DB_HOST","localhost");
+
+
+//tabla dbsistema
+
+define("DB_NAME","dbsistema");
+
+//usuario root
+define("DB_USERNAME","root");
+
+//contraseña no hay 
+define("DB_PASSWORD","");
+
+//utf8 tecnology
+
+define("DB_ENCODE","utf8");
+
+//
+
+define("SISTEMA_NOMBRE","POS_SYSTEM");
+
+
+
+
+?>
+
+
